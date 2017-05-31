e2db6363e4b68a8f3a57c0107d278ec776797921
WuMingyu <1wumingyu1@gmail.com>
2017-05-31 19:31:42 +0800

    Initial Commit

---
+++ SmartMonitor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SmartMonitor
+//
+//  Created by WuMingyu on 2017/5/31.
+//  Copyright © 2017年 WuMingyu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
